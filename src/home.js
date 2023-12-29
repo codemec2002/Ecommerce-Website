998df7aa694563eef2bb8542a8037ab01e2710ec
hupender <hupenderkhatod>
2023-12-29 19:18:43 +0530

    home page full

--- src/home.js
+++ src/home.js
@@ -2,14 +2,17 @@ import express from "express";
 const router = express.Router();
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import category_wise_name from "./get_category_name.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app=express();
 app.set('views',path.join(__dirname, '../views'));
-app.use(express.static(path.join(__dirname, '..', 'public')));
 
 router.get("/home", function(req,res) {
-    res.render("home.ejs");
+    res.render("home.ejs",{
+        categories: category_wise_name,
+    }
+    );
 })
 
 // router.listen("3000",()=> {
